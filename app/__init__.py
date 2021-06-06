9e466722b3b9518c8c30ecfe9377869a562490fb
mohammad shafiee <shafiee.mohamin@gmail.com>
2021-06-07 01:17:12 +0430

    added cors policy

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from .config import Config
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
+from .users.routes import users
+from .filters.routes import filters
+
+app.register_blueprint(users)
+app.register_blueprint(filters)
